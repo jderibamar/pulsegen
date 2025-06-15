8fa4d420af6247136bf802e6d56a113a34ad0164
jduarte8791 <j.duarte8791@gmail.com>
2025-06-14 22:13:24 -0300

    mudança de local do index.html

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')) )
 
 // Rota para o index.html
-app.get('/index', async (req, res) => 
+app.get('/', async (req, res) => 
 {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
